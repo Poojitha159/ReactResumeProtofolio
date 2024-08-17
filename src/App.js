4148140344f9b5353742f1fdbf737af535fc0424
Poojitha159 <makkenaharshini@gmail.com>
2024-08-17 17:59:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import Navbar from "./Components/Navbar/Navbar";
+import Intro  from "./Components/Intro/Intro";
+import FloatingDiv from "./Components/FloatingDiv/FloatingDiv";
+import Experience from "./Components/Experience/Experience";
+import Services from "./Components/Services/Services";
+import Works from "./Components/Works/Works";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonial from "./Components/Testimonials/Testimonial";
+import Footer from "./Components/Footer/Footer";
+import Contact from "./Components/Contact/Contact";
+import { themeContext } from "./Context";
+import { useContext } from "react";
 function App() {
+  const theme=useContext(themeContext);
+  const darkMode=theme.state.darkMode;
+  
   return (
-    <div className="App">
-      Subscribe ZainKeepsCode
+    <div className="App"
+    style={{bachground : darkMode?'black': '',
+      color : darkMode?'white': ''
+    }}
+    >
+
+
+     
+     <Navbar/>
+      <Intro/>
+      <Services/>
+      <FloatingDiv/>
+      <Experience/>
+      <Works/>
+      <Portfolio/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
+      
+     
     </div>
   );
 }
